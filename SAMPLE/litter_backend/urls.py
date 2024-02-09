f30ab89d04563bb0bbde976475d54838648268eb
Tim Engle <timothy.englew@gmail.com>
2024-02-09 13:29:14 -0800

    add classification update endpoint '/classification'

--- SAMPLE/litter_backend/urls.py
+++ SAMPLE/litter_backend/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 from litter_image_app import views
 
 urlpatterns = [
-    path('upload', views.upload, name='upload'),
+    path("upload", views.upload, name="upload"),
+    path("classification", views.update_img_classification, name="classification"),
 ]
